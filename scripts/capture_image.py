c344b6d30c7352d440f5c5332f12b0c7662b215f
Jan Henri Evard <evard@dai-lab.de>
2025-02-21 10:16:35 +0100

    added new samples

--- scripts/capture_image.py
+++ scripts/capture_image.py
@@ -4,15 +4,15 @@ import re
 import time
 
 # URL to capture the image from
-url_capture = "http://10.42.8.218/capture"
-url_capture_with_led = "http://10.42.8.218/capture_with_led"
+url_capture = "http://kitchen_cabinet_4/capture"
+url_capture_with_led = "http://kitchen_cabinet_4/capture_with_led"
 # Directory where images will be saved
-dir_path = "datasets/binaries/captured_images/"
+dir_path = "datasets/binaries/captured_images_home/"
 os.makedirs(dir_path, exist_ok=True)
 
 # Find the current maximum index of the captured images
 files = os.listdir(dir_path)
-pattern = re.compile(r"captured_image_(\d+)\.jpg")
+pattern = re.compile(r"captured_image_(\d+)\.jpg") 
 max_index = -1
 for f in files:
     match = pattern.match(f)
@@ -39,20 +39,20 @@ try:
 except requests.exceptions.RequestException as e:
     print(f"Failed to retrieve image: {e}")
 
-time.sleep(0.5)
-next_index = next_index + 1
-filename = f"captured_image_{next_index}.jpg"
-file_path = os.path.join(dir_path, filename)
+# time.sleep(0.5)
+# next_index = next_index + 1
+# filename = f"captured_image_{next_index}.jpg"
+# file_path = os.path.join(dir_path, filename)
 
-try:
-    response = requests.get(url_capture_with_led, stream=True)
-    response.raise_for_status()
+# try:
+#     response = requests.get(url_capture_with_led, stream=True)
+#     response.raise_for_status()
 
-    # Save the image
-    with open(file_path, "wb") as f:
-        for chunk in response.iter_content(chunk_size=8192):
-            f.write(chunk)
+#     # Save the image
+#     with open(file_path, "wb") as f:
+#         for chunk in response.iter_content(chunk_size=8192):
+#             f.write(chunk)
 
-    print(f"Image successfully saved as {file_path}")
-except requests.exceptions.RequestException as e:
-    print(f"Failed to retrieve image: {e}")
+#     print(f"Image successfully saved as {file_path}")
+# except requests.exceptions.RequestException as e:
+#     print(f"Failed to retrieve image: {e}")
